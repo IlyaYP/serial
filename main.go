b099fda9c56048f71a40d0eebde9fafca7178ba5
Ilya Suprun <pytok@yandex.ru>
2024-04-17 17:03:37 +0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   main.go
    	new file:   templates/index.html
    	new file:   templates/message.html

--- main.go
+++ main.go
@@ -3,9 +3,44 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"text/template"
 
 	"go.bug.st/serial"
 )
+
+var PORTS []string
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method not found", http.StatusNotFound)
+		return
+	}
+
+	// http.ServeFile(w, r, "templates/index.html")
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		log.Fatalf("template parsing: %s", err)
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 115200,
+		Parity:   serial.EvenParity,
+		DataBits: 7,
+		StopBits: serial.OneStopBit,
+	}
+
+	err = tmpl.Execute(w, PORTS)
+	if err != nil {
+		log.Fatalf("template execution: %s", err)
+	}
+
+}
 
 func main() {
 	ports, err := serial.GetPortsList()
@@ -18,4 +53,9 @@ func main() {
 	for _, port := range ports {
 		fmt.Printf("Found port: %v\n", port)
 	}
+	PORTS = ports
+
+	http.HandleFunc("/", serveIndex)
+	log.Fatal(http.ListenAndServe(":3001", nil))
+
 }
